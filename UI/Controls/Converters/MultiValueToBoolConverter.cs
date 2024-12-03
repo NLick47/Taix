5690b945cf4072f4af1a432d176ab11ace95e160
Nlick47 <2247717951@qq.com>
2024-12-03 23:29:52 +0800

    日期选择的一些样式更改

---
+++ UI/Controls/Converters/MultiValueToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Controls.Converters
+{
+    public class MultiValueToBoolConverter : IMultiValueConverter
+    {
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            foreach (var value in values)
+            {
+                if (value is bool booleanValue && booleanValue)
+                {
+                    return false; 
+                }
+            }
+            return true;
+        }
+    }
+}
