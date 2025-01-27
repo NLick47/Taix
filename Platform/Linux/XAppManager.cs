5304e782df3da9f3875b42dc4d56f38684d4927d
NLick47 <2247717951@qq.com>
2025-01-27 11:47:24 +0800

    x11部分实现测试

--- Platform/Linux/XAppManager.cs
+++ Platform/Linux/XAppManager.cs
@@ -1,8 +1,11 @@
 ﻿using SharedLibrary.Models.AppObserver;
 using SharedLibrary.Servicers;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +13,94 @@ namespace Linux
 {
     public class XAppManager : IAppManager
     {
+        private readonly IntPtr _display;
+        public XAppManager()
+        {
+            _display = Xlib.XOpenDisplay(IntPtr.Zero);
+        }
+        
         public AppInfo GetAppInfo(nint hwnd_)
         {
+            GetWindowProcessId(_display,hwnd_, out var processId);
             return AppInfo.Empty;
         }
+        private Dictionary<string, AppInfo> _apps;
+        private readonly ConcurrentDictionary<int, (string Name, DateTime LastChecked)> _processNameCache;
+        private int _outTime = 5000;
+         private bool GetWindowProcessId(IntPtr display, IntPtr handle, out int processId)
+        {
+            processId = 0;
+            IntPtr pidAtom = Xlib.XInternAtom(display, "_NET_WM_PID", true);
+            if (pidAtom == IntPtr.Zero)
+            {
+                return false;
+            }
+            IntPtr card32Atom = Xlib.XInternAtom(display, "CARD32", true);
+            // if (card32Atom == IntPtr.Zero)
+            // {
+            //     Console.WriteLine("无法获取 CARD32 原子");
+            //     return false;
+            // }
+
+            IntPtr actualType;
+            int actualFormat;
+            IntPtr nitems, bytesAfter;
+            IntPtr propReturn;
+
+            int result = Xlib.XGetWindowProperty(display, handle, pidAtom, IntPtr.Zero, new IntPtr(4), false,(IntPtr) Atom.AnyPropertyType, out actualType, out actualFormat, out nitems, out bytesAfter, out propReturn);
+            if (result != 0)
+            {
+                return false;
+            }
+
+            if (actualType == Xlib.XInternAtom(display, "CARD32", true) && actualFormat == 32 && nitems == 1)
+            {
+                processId = Marshal.ReadInt32(propReturn);
+                Xlib.XFree(propReturn);
+                return true;
+            }
+
+            Xlib.XFree(propReturn);
+            return false;
+        }
+
+        private string? GetAppProcessName(int processId_)
+        {
+            if (!_processNameCache.TryGetValue(processId_, out var val) ||
+                (DateTime.Now - val.LastChecked).TotalSeconds > 600)
+            {
+                using var process = Process.GetProcessById(processId_);
+                if (!string.IsNullOrEmpty(process?.ProcessName))
+                {
+                    _processNameCache[processId_] = (process.ProcessName, DateTime.Now);
+                }
+                return process?.ProcessName;
+            }
+            return _processNameCache[processId_].Name;
+        }
+
+        private string GetAppExecutablePath(string processName_)
+        {
+            var process = Process.GetProcessesByName(processName_).FirstOrDefault();
+            if (process != null)
+            {
+                return process.MainModule?.FileName;
+            }
+            return string.Empty;
+        }
+
+        private bool IsSystemComponent(string processName_)
+        {
+            bool isSys = sysProcessSet.Contains(processName_);
+            return isSys;
+        }
+        
+        private static readonly HashSet<string> sysProcessSet = new()
+        {
+            "ShellExperienceHost",
+            "StartMenuExperienceHost",
+            "SearchHost",
+            "LockApp"
+        };
     }
 }
--- Platform/Linux/XAppObserver.cs
+++ Platform/Linux/XAppObserver.cs
@@ -1,24 +1,78 @@
-﻿using SharedLibrary.Servicers;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
+using SharedLibrary.Event;
+using SharedLibrary.Servicers;
 
 namespace Linux
 {
     public class XAppObserver : IAppObserver
     {
         public event AppObserverEventHandler OnAppActiveChanged;
+        
+        private IntPtr _display;
+        private bool _isStart;
+        private readonly IAppManager _appManager;
+        private readonly IWindowManager _windowManager;
+
+        public XAppObserver(IAppManager appManager_, IWindowManager windowManager)
+        {
+            _appManager = appManager_;
+            _windowManager = windowManager;
+            _display = Xlib.XOpenDisplay(IntPtr.Zero);
+            if (_display == IntPtr.Zero)
+            {
+                throw new Exception("Failed to open display");
+            }
+        }
 
         public void Start()
         {
-            
+            if (_isStart)
+            {
+                return;
+            }
+            _isStart = true;
+            var defaultScreen = Xlib.XDefaultScreen(_display);
+            var rootWindow =  Xlib.XRootWindow(_display,defaultScreen);
+            Xlib.XSelectInput(_display, rootWindow, EventMask.FocusChangeMask);
+            _ = Task.Run(() => MonitorFocusChanges());
         }
 
         public void Stop()
         {
-            
+            if (!_isStart) return;
+            _isStart = false;
+            Xlib.XCloseDisplay(_display);
+        }
+
+      
+        private void MonitorFocusChanges()
+        {
+            var ev = Marshal.AllocHGlobal(24 * sizeof(long));
+                while (_isStart)
+                {
+                    if (Xlib.Pending(_display) > 0)
+                    {
+                        Xlib.XNextEvent(_display,  ev);
+                        var xevent = Marshal.PtrToStructure<XAnyEvent>(ev);
+                        // if (xevent.type == (int)Event.FocusIn)
+                        // {
+                        //     var args = GetAppInfoEventArgs(xevent.window, DateTime.Now);
+                        //     OnAppActiveChanged?.Invoke(this, args);
+                        // }
+                        Debug.Write(ev);
+                    }
+                }
+        }
+        private AppActiveChangedEventArgs GetAppInfoEventArgs(IntPtr handle_, DateTime activeTime_)
+        {
+            var app = _appManager.GetAppInfo(handle_);
+            var window = _windowManager.GetWindowInfo(handle_);
+            return new AppActiveChangedEventArgs(app, window, activeTime_);
         }
     }
 }
