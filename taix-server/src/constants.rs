603e643b6808d5d8a5c5677082a682b4f401a781
Nick47 <2247717951@qq.com>
2026-05-06 23:05:48 +0800

    ci测试

---
+++ taix-server/src/constants.rs
@@ -0,0 +1,33 @@
+pub const SECS_PER_MIN: i64 = 60;
+pub const MINS_PER_HOUR: i64 = 60;
+pub const SECS_PER_HOUR: i64 = 3_600;
+pub const SECS_PER_DAY: i64 = 86_400;
+
+pub const DEFAULT_HTTP_PORT: u16 = 37_091;
+pub const DEFAULT_WEBSOCKET_PORT: u16 = 8_908;
+pub const BROADCAST_CHANNEL_CAPACITY: usize = 128;
+pub const MAX_CONCURRENT_PIPE_CLIENTS: usize = 16;
+
+pub const MAX_LOG_RETENTION_DAYS: usize = 31;
+
+pub const PIPE_CONNECT_RETRY_INTERVAL_SECS: u64 = 1;
+pub const PIPE_CREATE_RETRY_INTERVAL_SECS: u64 = 3;
+
+pub const MIN_VALID_TIMESTAMP: i64 = 1_577_836_800;
+pub const TIMESTAMP_FUTURE_TOLERANCE_SECS: i64 = 60;
+pub const MIN_VALID_YEAR: i32 = 2_000;
+pub const FUTURE_TIMESTAMP_TOLERANCE_MINUTES: i64 = 5;
+
+pub const MAX_WEB_DURATION_SECS: i64 = SECS_PER_HOUR * 4;
+pub const MAX_SITE_DURATION_SECS: i64 = 3_153_600_000;
+pub const MAX_ADD_URL_ITERATIONS: usize = 48;
+pub const DAY_VIEW_THRESHOLD: i64 = 31;
+
+pub const ICON_DOWNLOAD_TIMEOUT_SECS: u64 = 10;
+pub const SVG_PREFIX_CHECK_LEN: usize = 256;
+pub const MAX_ICON_SIZE_PX: u32 = 256;
+
+pub const MIGRATION_BATCH_SIZE: usize = 1_000;
+
+pub const DEFAULT_FREQUENT_USE_NUM: i32 = 2;
+pub const DEFAULT_MORE_NUM: i32 = 11;
